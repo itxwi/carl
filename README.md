805158167c1dc016ffb0cbbcbf5b3bdfbb9d98e9
itxwi <w02.huang@surreyschools.ca>
2025-06-28 15:44:00 -0700

    inital

---
+++ README.md
@@ -0,0 +1,14 @@
+This is carl bot
+Things he will do:
+(done) dnd dice
+user id system
+-- Powerlevel
+-- Money
+-- Chathistory (with the bot)
+(sorta )ai chat gf
+hood slang response
+stock market
+pokemon and fighting
+gambling blackjack
+
+(done )take parameter inputs
---
+++ carl.py
@@ -0,0 +1,175 @@
+import discord
+from discord.ext import commands
+import json
+from groq import Groq
+import random
+import sys
+
+"""
+//=========//
+// Helpers //
+//=========//
+"""
+
+def readSecrets():
+    """
+    Returns a dictionary
+    """
+    with open('secrets.json') as file:
+        secrets = json.load(file)
+    return secrets
+
+def setIdentity(message, powerlevel = 0 ,money = 0, chathistory = []):
+    userinfo = {
+        "userid": message.author.id,
+        "power_level": powerlevel,
+        "money": money,
+        "chathistory": chathistory
+    }
+    p = json.dumps()
+
+def getIdentity():
+    pass
+
+"""
+//===============//
+// Initalization //
+//===============//
+"""
+
+# Discord
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# Groq
+groqclient = Groq(api_key=readSecrets()["groq_token"])
+
+"""
+//==========//
+// Helpers 2//
+//==========//
+"""
+
+def askGroq(question):
+    """
+    Takes in a string and returns a string
+    """
+    messages = [
+    {"role": "user", "content": f"{question}"}
+    ]
+    response = groqclient.chat.completions.create(
+    model="llama3-70b-8192",
+    messages=messages
+    ) 
+    return response.choices[0].message.content
+
+"""
+//==========//
+// Commands //
+//==========//
+"""
+
+class Dnd:
+    def __init__(self):
+        self.description = "this rolls a dice from 1-20"
+        self.power = 0
+    
+    async def command(self, message, parameter):
+        await message.channel.send(f"your number is {str(random.randrange(1,20))} cuh")
+        
+
+class Ping:
+    def __init__(self):
+        self.description = "replys your ping"
+        self.power = 0
+        
+    async def command(self, message, parameter):
+        latency = f"{client.latency:.2f} ms"
+        await message.channel.send(f"pong {latency}")
+
+class Commandlist:
+    def __init__(self):
+        self.description = "sends a list of commands"
+        self.power = 0
+    
+    async def command(self, message, parameter):
+        lisst = "```"
+        for command in commands:
+            lisst += f"{command} description: {commands[command].description}\n"
+            #await message.channel.send(f"{command} description: {commands[command].description}")
+        lisst += "```"
+        await message.channel.send(lisst)
+
+class Signup:
+    def __init__(self):
+        self.description = "signs the user up for somethin"
+    async def command(self, message, parameter):
+        await message.channel.send("beep boop boop beep... request accepted probably")
+        setIdentity(message)
+
+class Talk:
+    def __init__(self):
+        self.description = "talks with the bot using ai"
+        self.power = 1
+
+    async def command(self, message, parameter):
+        groqresponse = askGroq(' '.join(parameter))
+        await message.channel.send(groqresponse)
+
+        
+class Kys:
+    def __init__(self):
+        self.description = "turns off bot"
+        self.power = 5
+
+    async def command(self,message, parameter):
+        await message.channel.send(":C")
+        sys.exit()
+        
+commands = {
+    "dnd": Dnd(),
+    "ping": Ping(),
+    "commands":Commandlist(),
+    "signup": Signup(),
+    "talk": Talk(),
+    "kys": Kys(),
+}
+
+"""
+//=========//
+// Invokes //
+//=========//
+"""
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is online!')
+
+@client.event
+async def on_message(message):
+    
+    if message.author == client.user:
+        return #ignores self
+    
+
+    # Content processing
+    processed_message = message.content.lower()[5::].split(" ")
+    processed_command = processed_message[0]
+    processed_parameters = processed_message[1::]
+    if message.content.lower().startswith('carl!') and processed_command in commands.keys():
+
+        #get user power before doing anything
+
+        await commands[processed_command].command(message, processed_parameters)
+    
+
+
+"""
+//==============//
+// Finalization //
+//==============//
+"""
+
+client.run(readSecrets()["bot_token"])
